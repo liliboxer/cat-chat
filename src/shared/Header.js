21e0ec9c11c3541473aa093222ce94ec1d491a3e
Lili Boxer <ninaliboxer@gmail.com>
2019-06-03 16:36:36 -0700

    basic css

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,10 +1,19 @@
 import Component from '../Component.js';
+import Profile from './Profile.js';
 
 class Header extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
             <header>
-                <h1>Hello</h1>
+                <h1>Chat Room</h1>
             </header>
         `;
 
--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -3,7 +3,11 @@ import Component from '../Component.js';
 class Profile extends Component {
     renderTemplate() {
         return /*html*/`
-            
+            <div class="profile">
+                <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/08/15/12/smileyfaceemoji1508a.jpg?w968">
+                <span>Lili</span>
+                <button>Sign Out</button>
+            </div>
         `;
 
     }
