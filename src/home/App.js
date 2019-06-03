eab94fba63e6a74e71c688ca965a32e16d6fc996
Lili Boxer <ninaliboxer@gmail.com>
2019-06-03 15:13:21 -0700

    page linked up

---
+++ src/home/App.js
@@ -0,0 +1,12 @@
+import Component from '../Component.js';
+
+class App extends Component {
+    renderTemplate() {
+        return /*html*/`
+            
+        `;
+
+    }
+}
+
+export default App;
