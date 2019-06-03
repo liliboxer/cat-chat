b199a95beeb3e945681b9a5cbcafe69e5b364e1e
Lili Boxer <ninaliboxer@gmail.com>
2019-06-03 16:11:01 -0700

    check auth util in

---
+++ src/shared/Profile.js
@@ -0,0 +1,12 @@
+import Component from '../Component.js';
+
+class Profile extends Component {
+    renderTemplate() {
+        return /*html*/`
+            
+        `;
+
+    }
+}
+
+export default Profile;
